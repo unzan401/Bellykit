7b5cec2beb337f8404692345ebbaf361e5fdc541
unzan401 <55685430+unzan401@users.noreply.github.com>
2020-02-04 12:09:14 +0800

    login

--- Djangoweb/bellykitweb/settings.py
+++ Djangoweb/bellykitweb/settings.py
@@ -74,22 +74,22 @@ WSGI_APPLICATION = 'bellykitweb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 DATABASES = {
-    'default':{
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'bellykit_platform',
-        'USER': 'belly_engineer',
-        'PASSWORD': 'JK9948hubtLL2',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+# DATABASES = {
+#     'default':{
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'bellykit_platform',
+#         'USER': 'belly_engineer',
+#         'PASSWORD': 'JK9948hubtLL2',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#     }
+# }
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
